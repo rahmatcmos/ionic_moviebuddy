cd5cfad0c99325e1d4d539a901590f722c6fe3fd
Josh <jsgoldberg90@gmail.com>
2014-04-18 23:46:58 -0700

    add user details update

--- myApp/platforms/ios/www/js/profile.js
+++ myApp/platforms/ios/www/js/profile.js
@@ -1,2 +1,18 @@
-app.controller('ProfileController', function($scope) {
+app.controller('ProfileController', function($scope, $ionicSideMenuDelegate, $rootScope, updateUsers, getUsers) {
+
+  $scope.toggleRight = function(){
+    $ionicSideMenuDelegate.toggleRight();
+  };
+
+  $scope.updateUser = function(){
+    updateUsers.updateUser($rootScope.user.facebookId, $rootScope.user)
+    .then(function(){
+      getUsers.getUser($rootScope.user.facebookId)
+      .then(function(response){
+        $rootScope.user = response.data;
+        $ionicSideMenuDelegate.toggleRight();
+      });
+    });
+  };
+
 });
