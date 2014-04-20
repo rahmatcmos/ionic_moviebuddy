22dc162e00d270ee7d6af4b95e0d2440ea0c58d7
Josh <jsgoldberg90@gmail.com>
2014-04-19 19:39:08 -0700

    add logo

--- myApp/www/js/app.js
+++ myApp/www/js/app.js
@@ -23,7 +23,6 @@ app.config(function($stateProvider, $urlRouterProvider) {
   // Set up the various states which the app can be in.
   // Each state's controller can be found in controllers.js
   $stateProvider
-
     .state('login', {
       url: '/login',
       templateUrl: 'templates/login.html',
