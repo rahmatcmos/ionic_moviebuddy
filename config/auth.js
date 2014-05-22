f1a622a75fac5a52a74042bb155d8e45d998c2a7
Josh <jsgoldberg90@gmail.com>
2014-05-22 14:04:51 -0700

    intiail push to heroku

--- config/auth.js
+++ config/auth.js
@@ -1,8 +1,8 @@
 // config/auth.js
 
 exports.facebookAuth = {
-  'clientID': '227436327446392', // your App ID
-  'clientSecret': '9491a0d640daf987b3897361b7f19f1c', // your App Secret
+  'clientID': '225403147654068', // your App ID
+  'clientSecret': '0bb841a53a02a76e177abe3073ee54b9', // your App Secret
   'callbackURL': 'http://localhost:8080/auth/facebook/callback'
 };
 
